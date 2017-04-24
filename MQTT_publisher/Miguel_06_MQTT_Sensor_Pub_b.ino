02007d9e8b0881493feba93fdf2babd76b5cfb39
Midgie75 <miguel.setiabudi@gmx.net>
2017-04-24 21:25:25 +0200

    Add files via upload

---
+++ MQTT_publisher/Miguel_06_MQTT_Sensor_Pub_b.ino
@@ -0,0 +1,169 @@
+
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+// Sensor part
+
+#include <Wire.h>
+#include "Adafruit_VCNL4010.h"
+
+Adafruit_VCNL4010 vcnl;
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+
+// MQTT Example
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Mongoose";
+const char* password = "Mongoose";
+const char* mqtt_server = "192.168.4.1";
+
+WiFiClient PubClient;
+PubSubClient client(PubClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  //pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  
+  // Sensor part
+  Wire.begin(2,0);
+  Serial.println("VCNL4010 test");
+
+  if (! vcnl.begin()){
+    Serial.println("Sensor not found :(");
+    while (1);
+  }
+  Serial.println("Found VCNL4010");
+  //
+  
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/*void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+  
+}
+*/
+
+void reconnect() {
+  // Loop until we're reconnected
+  Serial.print("Pubclient attempting MQTT connection. ");
+  //while (!client.connected()) {
+  //  Serial.print(".");
+  //client.connect("VCNLPub");
+  // Attempt to connect
+  if (client.connect("VCNLPub")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+  } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+  }
+  
+  
+  //Serial.println("connected");
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    client.loop();
+    delay(2500);
+    long now = millis();
+    if (now - lastMsg > 5000) {
+      lastMsg = now;
+      //++value;
+      //snprintf (msg, 75, "hello world #%ld", value);
+      value = int(vcnl.readAmbient());
+      snprintf (msg,75, "%ld", value);
+      Serial.print("VCNL/Ambient: ");
+      Serial.println(value);
+      Serial.println(msg);
+      client.publish("VCNL/Ambient", msg);
+      value = int(vcnl.readProximity());
+      snprintf (msg,75, "%ld", value);
+      Serial.print("VCNL/Proximity: ");
+      Serial.println(value);
+      Serial.println(msg);
+      client.publish("VCNL/Proximity", msg);
+    }
+    //client.disconnect();
+  }
+}
